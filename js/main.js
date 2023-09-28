f9a99aec7ca51223208e771e5a5fa7b46202d401
MarkSerna <marcoesernal@gmail.com>
2023-09-27 20:07:28 -0500

    Se hicieron ajustes de archivos JSON
    Co-authored-by: Jeimy Pinto <JeimyPinto@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -45,9 +45,9 @@ showNavbar("header-toggle", "nav-bar", "body-pd", "header"); */
 function onFileChange(event) {
   var fileName = event.target.files[0].name;
   var reader = new FileReader();
-  reader.onload = function(e) {
-      var obj = JSON.parse(e.target.result);
-      displayData(obj, fileName);
+  reader.onload = function (e) {
+    var obj = JSON.parse(e.target.result);
+    displayData(obj, fileName);
   };
   reader.readAsText(event.target.files[0]);
 }
@@ -63,11 +63,25 @@ function displayData(data, fileName) {
   // Reemplaza el texto en el elemento div con el nombre del archivo
   nameElement.textContent = fileName;
 
+  // Verifica si data es un array
+  if (!Array.isArray(data)) {
+    // Si data es un objeto, conviértelo en un array de objetos
+    data = Object.values(data);
+    console.log(data);
+  }
   // Crea un nuevo elemento de lista para los datos del archivo
   var fileDataItem = document.createElement('li');
-  fileDataItem.textContent = JSON.stringify(data);
-  fileDataItem.style.fontSize = '12px';
-  fileDataItem.style.color = 'white';
+  
+  // Itera sobre cada objeto en los datos del archivo
+  data.forEach(function(item) {
+      var li = document.createElement('li');
+      li.textContent = 'Nombre: ' + item.nombre + ', posX: ' + item.posX + ', posY: ' + item.posY;
+      li.style.fontSize = '12px';
+      li.style.color = 'white';
+      fileDataItem.appendChild(li);
+      li.textContent = item;
+  });
+
 
   // Agrega el elemento de la lista debajo del elemento div
   nameElement.parentNode.insertBefore(fileDataItem, nameElement.nextSibling);
@@ -108,7 +122,7 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch(error => {
           console.error('Error al cargar los nodos desde el archivo JSON: ' + error);
-        });
+        });o
       // Llama a la función para crear un nodo con los datos y coordenadas proporcionados
       createNode(posX, posY);
     }
@@ -162,38 +176,57 @@ function createNode(posX, posY) {
   // Agrega la imagen al contenedor del mapa
   const mapContainer = document.getElementById("map-container");
   mapContainer.appendChild(image);
-
-  // Crea un objeto nodo con las coordenadas
-  const node = new Node(posX, posY);
-
-  // Envia los datos del nodo al archivo PHP para inserción en la base de datos
-  sendNodeToDatabase(node);
 }
-/**
- * Envía las coordenadas del nodo al archivo PHP para inserción en la base de datos.
- * @param {number} posX - La posición en el eje X.
- * @param {number} posY - La posición en el eje Y.
- */
-function sendNodeToDatabase(posX, posY) {
-  // Crea un objeto con las coordenadas del nodo
-  const nodeData = { posX, posY };
+function uploadDataToDatabase(data) {
+  // Requiere tu archivo connection.php
+  const { connection } = require_once('../connection.php');
 
-  // Envia los datos del nodo al archivo PHP para inserción
-  fetch('../nodes/insert_node.php', {
-    method: 'POST',
-    body: JSON.stringify(nodeData), // Solo envía las coordenadas
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(response => response.text())
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => {
-      console.error('Error al insertar el nodo en la base de datos: ' + error);
+  // Inserta los nodos (ubicaciones)
+  data.ubicaciones.forEach(ubicacion => {
+    const { nombre, posX, posY } = ubicacion;
+    const insertNodoQuery = 'INSERT INTO nodos (nombre, posX, posY) VALUES (?, ?, ?)';
+
+    connection.query(insertNodoQuery, [nombre, posX, posY], (err, result) => {
+      if (err) {
+        console.error('Error al insertar nodo: ' + err);
+      } else {
+        console.log('Nodo insertado correctamente. ID: ' + result.insertId);
+      }
     });
+  });
+
+  // Inserta las aristas (conexiones)
+  data.conexiones.forEach(conexion => {
+    const { ubicacion1, ubicacion2, peso } = conexion;
+    const insertAristaQuery = `
+      INSERT INTO aristas (nodo_inicio, nodo_fin, peso)
+      SELECT n1.id, n2.id, ? FROM nodos n1, nodos n2
+      WHERE n1.nombre = ? AND n2.nombre = ?
+    `;
+
+    connection.query(
+      insertAristaQuery,
+      [peso, ubicacion1, ubicacion2],
+      (err, result) => {
+        if (err) {
+          console.error('Error al insertar arista: ' + err);
+        } else {
+          console.log('Arista insertada correctamente. ID: ' + result.insertId);
+        }
+      }
+    );
+  });
+
+  // Cierra la conexión
+  connection.end(err => {
+    if (err) {
+      console.error('Error al cerrar la conexión: ' + err);
+    } else {
+      console.log('Proceso completado. Conexión cerrada.');
+    }
+  });
 }
+
 
 
 
